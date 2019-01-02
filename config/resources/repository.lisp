38ab497833b56a1abf8095eac039c324f375b5fd
Erika Pauwels <erika.pauwels@gmail.com>
2019-01-02 17:10:11 +0100

    Add logies model
    mu-cl-resource domain config for the logies-basis application profile as
    described on https://data.vlaanderen.be/doc/applicatieprofiel/logies-basis/

--- config/resources/repository.lisp
+++ config/resources/repository.lisp
@@ -1,34 +1,19 @@
 (in-package :mu-cl-resources)
 
-;;;;
-;; NOTE
-;; docker-compose stop; docker-compose rm; docker-compose up
-;; after altering this file.
-
-
-;;;;
-;; Describe the prefixes which you'll use in the domain file here.
-;; This is a short-form which allows you to write, for example,
-;; (s-url "http://purl.org/dc/terms/title")
-;; as (s-prefix "dct:title")
-
-;; (add-prefix "dct" "http://purl.org/dc/terms/")
-
-
-;;;;;
-;; The following is the commented out version of those used in the
-;; commented out domain.lisp.
-
-;; (add-prefix "dcat" "http://www.w3.org/ns/dcat#")
-;; (add-prefix "dct" "http://purl.org/dc/terms/")
-;; (add-prefix "skos" "http://www.w3.org/2004/02/skos/core#")
-
-
-;;;;;
-;; You can use the muext: prefix when you're still searching for
-;; the right predicates during development.  This should *not* be
-;; used to publish any data under.  It's merely a prefix of which
-;; the mu.semte.ch organisation indicates that it will not be used
-;; by them and that it shouldn't be used for permanent URIs.
-
+(add-prefix "schema" "http://schema.org/")
+(add-prefix "dct" "http://purl.org/dc/terms/")
+(add-prefix "skos" "http://www.w3.org/2004/02/skos/core#")
+(add-prefix "adms" "http://www.w3.org/ns/adms#")
+(add-prefix "locn" "http://www.w3.org/ns/locn#")
+(add-prefix "prov" "http://www.w3.org/ns/prov#")
+(add-prefix "wgs" "http://www.w3.org/2003/01/geo/wgs84_pos#")
+(add-prefix "generiek" "https://data.vlaanderen.be/ns/generiek#")
+(add-prefix "adres" "https://data.vlaanderen.be/ns/adres#")
+(add-prefix "organisatie" "https://data.vlaanderen.be/ns/organisatie#")
+(add-prefix "logies" "https://data.vlaanderen.be/ns/logies#")
+(add-prefix "geosparql" "http://www.opengis.net/ont/geosparql#")
+(add-prefix "foaf" "http://xmlns.com/foaf/0.1/")
+(add-prefix "vcard" "http://www.w3.org/2006/vcard/ns#")
+(add-prefix "regorg" "http://www.w3.org/ns/regorg#")
+(add-prefix "mu" "http://mu.semte.ch/vocabularies/core/")
 (add-prefix "ext" "http://mu.semte.ch/vocabularies/ext/")
