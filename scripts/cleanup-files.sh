d6a6fcb77e0689fdd3a8d5895cf917a971102111
Erika Pauwels <erika.pauwels@gmail.com>
2019-04-08 15:24:51 +0200

    Add cleanup script

---
+++ scripts/cleanup-files.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+KEEP_NB_OF_FILES=10
+FILES_DIR=../data/files/*
+
+drc=`which docker-compose`
+if [ $? -ne 0 ]; then
+    echo "ERROR: could not find docker executable";
+    exit -1;
+fi
+
+# Restart services
+$drc restart virtuoso resource import
+
+# Cleanup output files
+ls -d -1tr $FILES_DIR | head -n -$KEEP_NB_OF_FILES | xargs -d '\n' rm -f
