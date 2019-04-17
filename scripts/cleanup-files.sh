04af9cf22b1fa45468f63ce189565a38c856e820
Erika Pauwels <erika.pauwels@gmail.com>
2019-04-17 19:04:07 +0200

    Update cleanup script

--- scripts/cleanup-files.sh
+++ scripts/cleanup-files.sh
@@ -4,14 +4,8 @@ cd /home/tvladmin/linked.toerismevlaanderen.be/scripts
 KEEP_NB_OF_FILES=10
 FILES_DIR=../data/files/*
 
-drc=`which docker-compose`
-if [ $? -ne 0 ]; then
-    echo "ERROR: could not find docker executable";
-    exit -1;
-fi
-
 # Restart services
-$drc restart database resource import
+/usr/local/bin/docker-compose restart database resource import
 
 # Cleanup output files
 ls -d -1tr $FILES_DIR | head -n -$KEEP_NB_OF_FILES | xargs -d '\n' rm -f
