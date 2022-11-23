d49f72a4c69fd7154f6f9dd3dece39b6fa5f08fa
Erika Pauwels <erika.pauwels@gmail.com>
2022-11-23 16:50:17 +0100

    Move data to public graph instead of application graph

--- config/authorization/config.ex
+++ config/authorization/config.ex
@@ -6,53 +6,58 @@ alias Acl.GroupSpec, as: GroupSpec
 defmodule Acl.UserGroups.Config do
   def user_groups do
     [
-      # // PUBLIC
       %GroupSpec{
         name: "public",
         useage: [:read],
         access: %AlwaysAccessible{},
         graphs: [ %GraphSpec{
-                    graph: "http://mu.semte.ch/application",
-                    constraint: %ResourceConstraint{
-                      resource_types: [
-                        "http://purl.org/dc/terms/Agent",
-                        "http://schema.org/Rating",
-                        "https://data.vlaanderen.be/ns/logies#Beschrijving",
-                        "http://schema.org/CreativeWork",
-                        "https://data.vlaanderen.be/ns/logies#Faciliteit",
-                        "http://www.w3.org/ns/regorg#RegisteredOrganization",
-                        "https://data.vlaanderen.be/ns/logies#Kwaliteitslabel",
-                        "https://data.vlaanderen.be/ns/logies#Logies",
-                        "https://data.vlaanderen.be/ns/logies#MediaObject",
-                        "https://data.vlaanderen.be/ns/logies#Registratie",
-                        "https://data.vlaanderen.be/ns/logies#Ruimte",
-                        "https://data.vlaanderen.be/ns/logies#ToeristischeRegio",
-                        "https://data.vlaanderen.be/ns/logies#Verhuureenheid",
-                        "http://purl.org/dc/terms/Location",
-                        "http://purl.org/dc/terms/LicenseDocument",
-                        "http://www.w3.org/ns/locn#:Address",
-                        "http://schema.org/ContactPoint",
-                        "http://www.w3.org/ns/locn#:Geometry",
-                        "http://www.w3.org/ns/adms#Identifier",
-                        "http://schema.org/QuantitativeValue",
-                        "https://data.vlaanderen.be/ns/adres#Locatieaanduiding",
-                        "http://www.w3.org/2003/01/geo/wgs84_pos#Point",
-                        "http://www.w3.org/ns/prov#Generation",
-                        "http://www.w3.org/ns/prov#Invalidation",
-                        "http://mu.semte.ch/vocabularies/ext/StandardizedUnit",
-                        "http://mu.semte.ch/vocabularies/ext/Audience",
-                        "http://mu.semte.ch/vocabularies/ext/Language",
-                        "http://mu.semte.ch/vocabularies/ext/FileFormat",
-                        "http://mu.semte.ch/vocabularies/ext/LocatorDesignatorType",
-                        "http://mu.semte.ch/vocabularies/ext/RegistrationStatus",
-                        "http://mu.semte.ch/vocabularies/ext/RegistrationLodgingType",
-                        "http://mu.semte.ch/vocabularies/ext/RegistrationPublicationLodgingType",
-                        "http://mu.semte.ch/vocabularies/ext/RoomType"
-                      ]
-                    } } ] }
+          graph: "http://mu.semte.ch/graphs/public",
+          constraint: %ResourceConstraint{
+            resource_types: [
+              "http://purl.org/dc/terms/Agent",
+              "http://schema.org/Rating",
+              "https://data.vlaanderen.be/ns/logies#Beschrijving",
+              "http://schema.org/CreativeWork",
+              "https://data.vlaanderen.be/ns/logies#Faciliteit",
+              "http://www.w3.org/ns/regorg#RegisteredOrganization",
+              "https://data.vlaanderen.be/ns/logies#Kwaliteitslabel",
+              "https://data.vlaanderen.be/ns/logies#Logies",
+              "https://data.vlaanderen.be/ns/logies#MediaObject",
+              "https://data.vlaanderen.be/ns/logies#Registratie",
+              "https://data.vlaanderen.be/ns/logies#Ruimte",
+              "https://data.vlaanderen.be/ns/logies#ToeristischeRegio",
+              "https://data.vlaanderen.be/ns/logies#Verhuureenheid",
+              "http://purl.org/dc/terms/Location",
+              "http://purl.org/dc/terms/LicenseDocument",
+              "http://www.w3.org/ns/locn#:Address",
+              "http://schema.org/ContactPoint",
+              "http://www.w3.org/ns/locn#:Geometry",
+              "http://www.w3.org/ns/adms#Identifier",
+              "http://schema.org/QuantitativeValue",
+              "https://data.vlaanderen.be/ns/adres#Locatieaanduiding",
+              "http://www.w3.org/2003/01/geo/wgs84_pos#Point",
+              "http://www.w3.org/ns/prov#Generation",
+              "http://www.w3.org/ns/prov#Invalidation",
+              "http://mu.semte.ch/vocabularies/ext/StandardizedUnit",
+              "http://mu.semte.ch/vocabularies/ext/Audience",
+              "http://mu.semte.ch/vocabularies/ext/Language",
+              "http://mu.semte.ch/vocabularies/ext/FileFormat",
+              "http://mu.semte.ch/vocabularies/ext/LocatorDesignatorType",
+              "http://mu.semte.ch/vocabularies/ext/RegistrationStatus",
+              "http://mu.semte.ch/vocabularies/ext/RegistrationLodgingType",
+              "http://mu.semte.ch/vocabularies/ext/RegistrationPublicationLodgingType",
+              "http://mu.semte.ch/vocabularies/ext/RoomType"
+            ]
+          } } ]
+      },
+
+      # // CLEANUP
+      #
+      %GraphCleanup{
+        originating_graph: "http://mu.semte.ch/application",
+        useage: [:write],
+        name: "clean"
+      }
     ]
   end
 end
-
-
-
