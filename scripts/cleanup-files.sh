686dced7679012078fc2b0a488d76a97dd3f783f
Erika Pauwels <erika.pauwels@gmail.com>
2023-02-13 10:59:54 +0100

    Fix service names in cleanup-files script

--- scripts/cleanup-files.sh
+++ scripts/cleanup-files.sh
@@ -5,7 +5,7 @@ KEEP_NB_OF_FILES=10
 FILES_DIR=../data/files/*
 
 # Restart services
-/usr/local/bin/docker-compose restart database resource import
+/usr/local/bin/docker-compose restart triplestore resource
 
 # Cleanup output files
 ls -d -1tr $FILES_DIR | head -n -$KEEP_NB_OF_FILES | xargs -d '\n' rm -f
