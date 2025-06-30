22b8fa8b63a0c1abf7e615cf8ca4dd0f13f5ab5d
Erika Pauwels <erika.pauwels@gmail.com>
2025-06-30 20:18:24 +0200

    Upgrade docker command in cleanup script

--- scripts/cleanup-files.sh
+++ scripts/cleanup-files.sh
@@ -5,7 +5,7 @@ KEEP_NB_OF_FILES=100
 FILES_DIR=../data/files/*
 
 # Restart services
-/usr/local/bin/docker-compose restart triplestore resource
+/usr/bin/docker compose restart triplestore resource
 
 # Cleanup output files
 ls -d -1tr $FILES_DIR | head -n -$KEEP_NB_OF_FILES | xargs -d '\n' rm -f
